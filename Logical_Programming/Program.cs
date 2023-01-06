ad9709bd7cec780392a1d512acaa2ce4a8fd41cc
Rushikesh041 <jadhavrushi041@gmail.com>
2023-01-06 22:59:12 +0530

    [Rushi]CouponNumbers

--- Logical_Programming/Program.cs
+++ Logical_Programming/Program.cs
@@ -6,16 +6,10 @@ namespace Logical_Programming
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Number for how many times to print the statement to get time elapsed for it : ");
-            int s = Convert.ToInt32(Console.ReadLine());
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
-            for (int i = 0; i < s; i++)
-            {
-                Console.WriteLine("Hello Rushikesh Jadhav");
-            }
-            stopWatch.Stop();
-            Console.WriteLine("Time Elapsed : {0}", stopWatch.Elapsed);
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            Random random = new Random();
+            string result = new string(Enumerable.Repeat(chars, 8).Select(s => s[random.Next(s.Length)]).ToArray());
+            Console.WriteLine("Coupon Number is : " + result);
         }
     }
 
