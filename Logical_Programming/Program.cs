69e6e48d177a891faed63e82bfaa633fa2fd0bde
Rushikesh041 <jadhavrushi041@gmail.com>
2023-01-06 19:29:03 +0530

    [Rushi]Fibo

--- Logical_Programming/Program.cs
+++ Logical_Programming/Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace Logical_Programming
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wel Come to Logical Programming");
+            Console.WriteLine("Enter the Number of Elements : ");
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            int num1 = 0, num2 = 1, num3;
+
+            Console.Write("Fibonacci Series is : ");
+            for (int i = 1; i <= Number; i++)
+            {
+                num3 = num1 + num2;
+                Console.Write(num3 + " ");
+                num1 = num2;
+                num2 = num3;
+            }
+
         }
     }
 }
