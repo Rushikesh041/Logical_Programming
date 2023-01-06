1a43c65025abb76ab40bc693154ddb8c61b1a465
Rushikesh041 <jadhavrushi041@gmail.com>
2023-01-06 21:28:23 +0530

    [Rushi]Reverse_Number

--- Logical_Programming/Program.cs
+++ Logical_Programming/Program.cs
@@ -4,26 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Number to Check Number is Perfect or Not : ");
-            int Number = Convert.ToInt32(Console.ReadLine());
-            int sum = 0;
+            Console.WriteLine("Enter the Number to get Reverse Number : ");
+            int ReNum = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 1; i < Number; i++)
-            {
-                if (Number % i == 0)
-                {
-                    sum = sum + i;
-                }
-            }
-            if (sum == Number)
-            {
-                Console.WriteLine(Number + " is Perfect Number");
+            int Reverse = 0, rem;
 
-            }
-            else 
+            while (ReNum != 0)
             {
-                Console.WriteLine(Number + " is Not Perfect Number");
+                rem = ReNum % 10;
+                Reverse = (Reverse * 10) + rem;
+                ReNum /= 10;
             }
+            Console.WriteLine("Reverse Number is : " + Reverse);
+
         }
 
     }
